731300119628709d808feec5308eafe63e6e426a
ductaip <dtai858@gmail.com>
2025-03-31 06:00:41 +0700

    controller: config controller using anotation mapping and test swagger

--- src/main/java/be_java_learning/controller/UserController.java
+++ src/main/java/be_java_learning/controller/UserController.java
@@ -1,10 +1,7 @@
 package be_java_learning.controller;
 
 import be_java_learning.dto.request.UserRequestDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -13,4 +10,11 @@ public class UserController {
     public String addUser(@RequestBody UserRequestDTO userDTO) {
         return "User added";
     }
+
+    @PutMapping("/{userId}")
+    public String updateUser(@PathVariable("userId") int id, @RequestBody UserRequestDTO userDTO) {
+        System.out.println("Request update userId=" + id);
+        return "User with id: " + id + " have updated successfully";
+    }
+
 }
