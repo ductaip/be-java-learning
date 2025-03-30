b8a74200a53c4666d39905257d6657b3e3db6408
ductaip <dtai858@gmail.com>
2025-03-31 05:08:01 +0700

    initial for learning java spring

---
+++ src/main/java/be_java_learning/BeJavaLearningApplication.java
@@ -0,0 +1,13 @@
+package be_java_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be_java_learning/BeJavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package be_java_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
