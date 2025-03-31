14574bd0d8723913f920f87c44d4fd732ba95231
ductaip <dtai858@gmail.com>
2025-03-31 14:59:20 +0700

    add some controller method with annotation

--- src/main/java/be_java_learning/controller/UserController.java
+++ src/main/java/be_java_learning/controller/UserController.java
@@ -3,6 +3,8 @@ package be_java_learning.controller;
 import be_java_learning.dto.request.UserRequestDTO;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -12,9 +14,35 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable("userId") int id, @RequestBody UserRequestDTO userDTO) {
-        System.out.println("Request update userId=" + id);
-        return "User with id: " + id + " have updated successfully";
+    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO userDTO) {
+        System.out.println("Request update userId=" + userId);
+        return "User with userId: " + userId + " have updated successfully";
     }
 
+    @PatchMapping("/{userId}")
+    public String changeStatus(@PathVariable int userId, @RequestParam(required = false) boolean status) {
+        System.out.println("Request update userId = " + userId);
+        return "User status changed to status: " + status;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable int userId) {
+        System.out.println("Request delete userId = " + userId);
+        return "Deleted user with id: " + userId + " successfully";
+    }
+
+    @GetMapping("/{userId}")
+    public UserRequestDTO getUser(@PathVariable int userId) {
+        System.out.println("Get detail info of userId : " + userId);
+        return new UserRequestDTO("Tempest", "Rimuru", "phone123", "mock_email");
+    }
+
+    @GetMapping("/list")
+    public List<UserRequestDTO> getAllUser(
+            @RequestParam(defaultValue = "0") int pageNo,
+            @RequestParam(defaultValue = "20") int pageSize
+    ) {
+        System.out.println("Request to get all list successfully with pageNumber = " + pageNo + " and page size = " + pageSize);
+        return List.of(new UserRequestDTO("Tempest", "Rimuru", "phone123", "mock_email"), new UserRequestDTO("Guy", "Suranka", "newphone", "new_mail"));
+    }
 }
